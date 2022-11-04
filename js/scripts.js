136ed38f1e17b9222d78311a18be2afa451f4c81
Alex Johnson <alex.johnson293@gmail.com>
2022-11-04 09:56:34 -0700

    pass fith TEST

--- js/scripts.js
+++ js/scripts.js
@@ -14,7 +14,18 @@ PizzaOven.prototype.addSize = function(size) {
   this.size = size;
 }
 
-
+PizzaOven.prototype.pizzaPrice = function(pizzaToPrice) {
+  let baseCost = 15
+  let toppingCost = pizzaToPrice.toppings.length
+  if (pizzaToPrice.size === "x-large") {
+    baseCost = 26.00
+  } else if (pizzaToPrice.size === "large") {
+    baseCost = 21.00
+  }
+  let price = baseCost + (toppingCost * 0.75)
+  this.price = price  
+}
+  
 
 
 // ui logik
@@ -26,6 +37,6 @@ function handleFormSubmission() {
 }
 
 
-window.addEventListener("load", function() {
-  document.querySelector("form#id").addEventListener("submit", handleFormSubmission)
-})
+// window.addEventListener("load", function() {
+//   document.querySelector("form#id").addEventListener("submit", handleFormSubmission)
+// })
